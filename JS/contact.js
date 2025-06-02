0ddf59dfda6e617574007632529b9237f5dfa307
AlexisLaf <150516817+AlexisLaf@users.noreply.github.com>
2025-06-02 17:24:06 +0200

    Contact onsubmit

--- JS/contact.js
+++ JS/contact.js
@@ -1,7 +1,21 @@
 const satisfactionslider=document.getElementById("Satisfaction");
 const output=document.getElementById("satisfactionresult");
+const form=document.getElementById("contact");
+const response=document.getElementById("response");
+//This next line is to hide the output until it is displayed.
+response.style.marginLeft="-100%";
 
 satisfactionslider.addEventListener("input",Outputchange);
+
+form.addEventListener("submit", e=>{Formoutput(e)})
+
+function Formoutput(event){
+    event.preventDefault();
+    form.reset();
+
+    response.style.marginLeft="";
+    response.innerHTML="<br>We have received your informations.<br><br>We will contact you in the following weeks.<br><br><div class=\"inline\"><h3>Expected time : </h3><h1>"+Math.floor(Math.random()*6+1)+" Business days</h1></div><h6>(Disclaimer) The text above may be inaccurate as of the 3rd of Jume 2025 as the relevent sections to handle the work may have closed down.";
+}
 
 function Outputchange(){
     switch(satisfactionslider.value){
